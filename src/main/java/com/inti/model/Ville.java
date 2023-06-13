936ac6dd4b2aeb930f693d760f5b7b1983d308c1
Thibault Sanclemente <thibault_sanclemente@icloud.com>
2023-06-13 15:27:42 +0200

    Ajout classe ville

---
+++ src/main/java/com/inti/model/Ville.java
@@ -0,0 +1,46 @@
+package com.inti.model;
+
+public class Ville {
+
+	private int idVille;
+	private String nom;
+	
+	public Ville() {
+		super();
+	}
+
+	public Ville(String nom) {
+		super();
+		this.nom = nom;
+	}
+
+	public Ville(int idVille, String nom) {
+		super();
+		this.idVille = idVille;
+		this.nom = nom;
+	}
+
+	public int getIdVille() {
+		return idVille;
+	}
+
+	public void setIdVille(int idVille) {
+		this.idVille = idVille;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	@Override
+	public String toString() {
+		return "Ville [idVille=" + idVille + ", nom=" + nom + "]";
+	}
+	
+	
+	
+}
