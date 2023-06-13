9e99bff1732896cf6d1e9545d5c8092c63aea524
dsoz34 <sofiane.antibes@gmail.com>
2023-06-13 14:33:38 +0200

    reservation v1

--- src/main/java/com/inti/model/Reservation.java
+++ src/main/java/com/inti/model/Reservation.java
@@ -2,11 +2,12 @@ package com.inti.model;
 
 import java.time.LocalDate;
 
+
+
 public class Reservation {
 
 	private int idReservation;
 	private LocalDate date;
-	
 	
 	private int numero;
 
@@ -35,6 +36,30 @@ public class Reservation {
 	@Override
 	public String toString() {
 		return "Reservation [idReservation=" + idReservation + ", date=" + date + ", numero=" + numero + "]";
+	}
+
+	public int getIdReservation() {
+		return idReservation;
+	}
+
+	public void setIdReservation(int idReservation) {
+		this.idReservation = idReservation;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
 	}
 
 	
