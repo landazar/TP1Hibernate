228dadd5f9f196d032e98bd7f1a2e548ac3e7f20
sarahborkan <sarah.behaz1@gmail.com>
2023-06-13 16:32:10 +0200

    commit local

--- src/main/java/com/inti/model/Passager.java
+++ src/main/java/com/inti/model/Passager.java
@@ -1,7 +1,17 @@
 package com.inti.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
 public class Passager {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idPassager;
 	private String nom;
 	private String prenom;
